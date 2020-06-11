cb77da88ccfecdecf62df6c13285d027ad2a7012
Serhiy Prikhodko <serhii.prikhodko@gmail.com>
2020-06-11 19:17:02 +0300

    Initial Commit

---
+++ instagram-like-feed-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagram-like-feed-app
+//
+//  Created by Serhiy Prikhodko on 11.06.2020.
+//  Copyright © 2020 Serhiy Prikhodko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ instagram-like-feed-appTests/instagram_like_feed_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  instagram_like_feed_appTests.swift
+//  instagram-like-feed-appTests
+//
+//  Created by Serhiy Prikhodko on 11.06.2020.
+//  Copyright © 2020 Serhiy Prikhodko. All rights reserved.
+//
+
+import XCTest
+@testable import instagram_like_feed_app
+
+class instagram_like_feed_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
